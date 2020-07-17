778446f2106fb3a2b8917cce7f7dcad6d73e7ec2
ChrisKaczynski <krzysztof.kaczynski@vizlib.com>
2020-07-17 16:58:40 +0200

    logIn LogOut

--- ngrx-course-1-start/src/app/auth/reducers/index.ts
+++ ngrx-course-1-start/src/app/auth/reducers/index.ts
@@ -3,14 +3,32 @@ import {
   ActionReducerMap,
   createFeatureSelector,
   createSelector,
-  MetaReducer
+  MetaReducer,
+  createReducer,
+  on
 } from '@ngrx/store';
 
+import {AuthActions} from '../action-types'
+import { User } from '../model/user.model';
 
-export interface State {
+export interface AuthState {
+  user: User
 
 }
 
-export const reducers: ActionReducerMap<State> = {
-
+export const initialAuthState: AuthState ={
+  user: undefined
 };
+
+
+
+export const AuthReducer = createReducer(
+  initialAuthState,
+  on(AuthActions.login, (state, action) => {
+    return {
+      user: action.user
+    }
+  })
+);
+
+
